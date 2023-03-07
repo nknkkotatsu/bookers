adb4035a8b8d01385c7fa52d5cb3eea108b44d6b
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-03-07 09:41:46 +0000

    [Update]リンクの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'books/new'
   root to: "books#top"
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
+  get 'books' => 'books#index',as:'index'
+  get 'books/:id' => 'books#show',as:'show'
+  get 'books/:id/edit' => 'books#edit',as:'edit'
+  patch 'books/:id' => 'books#update',as:'update'
+  delete 'books/:id' => 'books#destroy',as:'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
